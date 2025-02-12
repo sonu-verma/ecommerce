e178fabd3d90ec5d8d3f559a9a0ce51a80fa2212
Sonu Verma <sonu.verma0715@gmail.com>
2025-02-12 17:38:33 +0530

    theme setup with laravel with user and admin dashboard

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Laravel Ecommerce + React Project
+# Laravel E-commerce + React Project
 
 # PHP image processing library
     -> Intervention Image
+
+DB_COLLATION: utf8mb4_unicode_ci
+
+
+# Setup Theme for E-commerce 
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth'])->group(function(){
+    Route::get("account-dashboard", [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth', AuthAdmin::class])->group(function(){
+    Route::get("admin", [AdminController::class, 'index'])->name('admin.index');
 });
